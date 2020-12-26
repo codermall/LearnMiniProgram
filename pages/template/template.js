cc6bf65068aef8a04b75f632826819c828eff369
frontendmal <226866318@qq.com>
2020-12-26 23:36:26 +0800

    03_wxs

---
+++ pages/template/template.js
@@ -0,0 +1,67 @@
+// pages/template/template.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movies: [['泰坦尼克号', '钢琴家', '手撕鬼子', '盗梦空间', '小王子'], [1,2,3,4],[11,22,33,44],[111,222,333,444]],
+    info: {btn: '按钮', value: '这是一个按钮模板'}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
