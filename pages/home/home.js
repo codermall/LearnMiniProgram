a7271159b9e9e95b54d90ff5438bce60b0c06bc1
frontendmal <226866318@qq.com>
2021-01-09 23:40:38 +0800

    界面API->弹窗

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,56 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleToast() {
+    wx.showToast({
+      title: '成功',
+      mask: true,
+      duration: 3000
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleModal() {
+    wx.showModal({
+      cancelColor: 'cancelColor',
+      title: '提示的标题',
+      content: '提示的内容',
+      success(res) {
+        if(res.confirm) {
+          console.log('点击了确定按钮');
+        } else if(res.cancel) {
+          console.log('点击了取消按钮');
+        }
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  handleLoading() {
+    wx.showLoading({
+      title: '加载ing',
+      mask: true
+    })
 
+    setTimeout(() => {
+      wx.hideLoading()
+    }, 3000)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleActionSheet() {
+    wx.showActionSheet({
+      itemList: ['a', 'b', 'c'],
+      success(res) {
+        console.log(res.tapIndex, res);
+      },
+      fail(res) {
+        console.log(res.errMsg);
+      }
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage(options) {
+    return {
+      title: '分享标题 title',
+      path: '/pages/home/home',
+      imageUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic.jj20.com%2Fup%2Fallimg%2F1113%2F041620103S8%2F200416103S8-4-1200.jpg&refer=http%3A%2F%2Fpic.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1612777312&t=4bc3c2665f5eadedec7cc834f5b47cb3'
+    }
   }
 })
