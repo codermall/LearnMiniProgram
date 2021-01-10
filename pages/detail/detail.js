07fbe965fa63e9a91dce6208c575cdb8a9ccd411
frontendmal <226866318@qq.com>
2021-01-10 23:17:35 +0800

    09_页面跳转

---
+++ pages/detail/detail.js
@@ -0,0 +1,26 @@
+// pages/detail/detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(123, options);
+  },
+
+  onUnload() {
+    var page = getCurrentPages()
+    var prevPage = page[page.length - 2]
+    var currPage = page[page.length - 1]
+    prevPage.setData({
+      title: 'hhh'
+    })
+  }
+})
